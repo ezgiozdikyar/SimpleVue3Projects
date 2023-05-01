6ef3b89c0d5641ac315872c3d0599be92eff5253
ezgi <ezgiozdikyar@gmail.com>
2023-05-01 16:36:03 +0300

    Add project files.

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<script>
+import Block from "./components/Block.vue";
+import Result from "./components/Result.vue";
+export default {
+  name: "App",
+  components: { Block, Result },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResult: false
+    };
+  },
+  methods: {
+    start() {
+      this.isPlaying = true;
+      this.delay = 2000 + Math.random() * 5000;
+      this.showResult = false;
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime;
+      this.isPlaying = false;
+      this.showResult = true;
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h1>Timer Game</h1>
+    <button @click="start" :disabled="isPlaying">Play</button>
+    <Block v-if="isPlaying" :delay="delay" @end="endGame"></Block>
+    <Result v-if="showResult" :score="score"></Result>
+  </div>
+</template>
+
+<style >
+#app {
+  text-align: center;
+  color: aquamarine;
+  margin-top: 60px;
+}
+button {
+  background-color: aquamarine;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button[disabled]{
+  opacity: 0.2;
+  cursor: not-allowed;
+}
+</style>
